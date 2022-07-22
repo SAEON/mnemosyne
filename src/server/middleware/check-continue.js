c364de8c453e1e4185c053eccf91b7708ab5402c
Zach Smith <zachsmith.sa@gmail.com>
2022-07-22 09:24:05 +0200

    Updated node server to respect cURL Expect heade and fail quickly on 409

--- src/server/middleware/check-continue.js
+++ src/server/middleware/check-continue.js
@@ -1,8 +1,31 @@
-/**
- * Always say 'yes'
- */
-export default function (req, res) {
+import parseResource from './parse-resource.js'
+import { access } from 'fs/promises'
+
+export default async function (req, res) {
   const server = this
+
+  const ctx = { req, res }
+  await parseResource.call(ctx)
+
+  const {
+    resource: { absolutePath },
+  } = ctx
+
+  // Check if the resource exists
+  const exists = await access(absolutePath)
+    .then(() => true)
+    .catch(() => false)
+
+  // If it exists return 409
+  if (exists) {
+    const msg = 'Conflict. Upload path already exists'
+    res.writeHead(409, msg)
+    res.write(msg)
+    res.end()
+    return
+  }
+
+  // Otherwise continue the request
   res.writeContinue()
   server.emit('request', req, res)
 }
